c02efeb61ece3648b95f9e2cd5e730ee5ad73d01
Mariusz Kusio <kusio1996@gmail.com>
2024-01-11 11:56:56 +0100

    commit all repository files

---
+++ tandem.js
@@ -0,0 +1,28 @@
+function changeh1() {
+    var xDiv = document.getElementById('barbergalery1');
+
+    if (xDiv.style.height == '')
+        xDiv.style.height = '650px'
+    else
+        xDiv.style.height = ''
+}
+
+
+
+function changeh2() {
+    var xDiv = document.getElementById('barbergalery2');
+
+    if (xDiv.style.height == '')
+        xDiv.style.height = '650px'
+    else
+        xDiv.style.height = ''
+}
+
+function changeh3() {
+    var xDiv = document.getElementById('barbergalery3');
+
+    if (xDiv.style.height == '')
+        xDiv.style.height = '650px'
+    else
+        xDiv.style.height = ''
+}
